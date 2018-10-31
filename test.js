107176e538326a71bcb8043f1709ae9af8967936
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-31 23:44:15 +0200

    Add failling test

---
+++ test.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React, { useEffect } from "react";
+import { create as render } from "react-test-renderer";
+
+it("calls effect", () => {
+  return new Promise(resolve => {
+    render(<EffectfulComponent effect={resolve} />);
+  });
+});
+
+function EffectfulComponent({ effect }) {
+  useEffect(effect);
+
+  return null;
+}
